81031a2d2b308cc439aa4fe179cf7992f226b958
jerep <115798732+jerempa@users.noreply.github.com>
2024-06-24 15:19:37 +0300

    Committed the script, requirements and gitignore and modified readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # powerbi-report-exporter
+
+This script generates and downloads Power BI reports through the Power BI REST Api. Concurrent tasks can be run with concurrent.futures. Script was used for testing purposes. A new directory, downloaded_reports, is created for the PDFs which are downloaded to test that the script works as it shoulds.
+
+## ids.txt and business_ids.csv
+
+The script uses local files ids.txt for client_id, client_secret, tenant_id, group(workspace)_id, report_id and bearer token. The txt-file is formatted as follows:
+client_id,val
+client_secret,val
+tenant_id,val
+group_id_dev,val
+report_id_pdf_dev,val
+bearer,val,expiry as datetime
+
+and business_ids.csv as follows:
+123
+123k
+321
+321k
+
+where newline is used as separator and "k" used to distinguish between not-concern and concern.
+
+## Installing libraries
+
+requirements.txt has needed libraries and their versions, though requests is the only one not part of Python standard library. This can be installed by running "pip install -r requirements.txt" on terminal.
---
+++ report-exporter.py
@@ -0,0 +1,290 @@
+import requests
+import time
+import concurrent.futures
+import logging
+from datetime import datetime, timedelta
+import os
+import csv
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(message)s')
+
+class ReportExporter:
+    """
+    A class to export reports from Power BI.
+
+    Attributes
+    ----------
+    bearer : str
+        The bearer token for authentication.
+    group_id : str
+        The group ID for the Power BI workspace.
+    report_id : str
+        The report ID for the Power BI report.
+    """
+    
+    def __init__(self, bearer, group_id, report_id):
+        """
+        Initializes the ReportExporter with the bearer token, group ID, and report ID.
+
+        Parameters
+        ----------
+        bearer : str
+            The bearer token for authentication.
+        group_id : str
+            The group ID for the Power BI workspace.
+        report_id : str
+            The report ID for the Power BI report.
+        """
+        self.bearer = bearer
+        self.group_id = group_id
+        self.report_id = report_id
+
+    def get_export_id(self, business_id: str, language: str) -> str | None:
+        """
+        Requests an export ID for a report.
+
+        Parameters
+        ----------
+        business_id : str
+            The business ID to filter the report.
+        language : str
+            The language for the report At the moment only affects the thousands separator (space or comma) but later also the texts.
+
+        Returns
+        -------
+        str | None
+            The export ID if the request was successful, otherwise None.
+        """
+        url = f'https://api.powerbi.com/v1.0/myorg/groups/{self.group_id}/reports/{self.report_id}/exportTo'
+
+        company_filter = f"CompanyBasicInfo/business_id_k eq '{business_id}'"
+
+        body = {
+            "format": "PDF",
+            "powerBIReportConfiguration": {
+                "reportLevelFilters": [{"filter": company_filter}],
+                "settings": {
+                    "locale": language
+                }
+            }
+        }
+
+        headers = {"Authorization": f'Bearer {self.bearer}'}
+
+        response = requests.post(url, json=body, headers=headers)
+        response.raise_for_status()
+
+        res_json = response.json()
+        if response.status_code == 202:
+            self.generate_report(res_json["id"], business_id, language)
+            return res_json["id"]
+        else:
+            return None
+
+    def generate_report(self, export_id: str, business_id: str, language: str) -> None:
+        """
+        Polls the report generation status and downloads the report when it's ready.
+
+        Parameters
+        ----------
+        export_id : str
+            The export ID for the report. NOTE: the export id is valid for 24 hours, so the reports could be generated once a day and the website could have the download request.
+        business_id : str
+            The business ID used for filtering the report.
+        language : str
+            The language setting for the report.
+
+        Returns
+        -------
+        None
+        """
+        url = f'https://api.powerbi.com/v1.0/myorg/groups/{self.group_id}/reports/{self.report_id}/exports/{export_id}'
+
+        headers = {"Authorization": f'Bearer {self.bearer}'}
+
+        start = time.time()
+        logging.info("Generation started")
+        while True:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+
+            res_json = response.json()
+            if res_json["status"] == "Succeeded":
+                end = time.time()
+                logging.info(f'Report generation took: {end - start:.2f} seconds')
+                self.download_report(export_id, business_id, language)
+                break
+            time.sleep(5)  # Polling the generation to avoid unnecessary amount of requests
+
+    def download_report(self, export_id: str, business_id: str, language: str) -> None:
+        """
+        Downloads the generated report.
+
+        Parameters
+        ----------
+        export_id : str
+            The export ID for the report.
+        business_id : str
+            The business ID used for filtering the report.
+        language : str
+            The language setting for the report.
+
+        Returns
+        -------
+        None
+        """
+        url = f'https://api.powerbi.com/v1.0/myorg/groups/{self.group_id}/reports/{self.report_id}/exports/{export_id}/file'
+
+        headers = {"Authorization": f'Bearer {self.bearer}'}
+
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        if os.path.basename(os.getcwd()) != "downloaded_reports":
+            try:
+                os.chdir(f'{os.getcwd()}/downloaded_reports')
+            except FileNotFoundError:
+                os.makedirs("downloaded_reports")
+                os.chdir(f'{os.getcwd()}/downloaded_reports') #Creating a separate directory for the downloaded PDFs
+
+        with open(f'downloaded_file_{business_id}_{language}.pdf', 'wb') as file:
+            file.write(response.content)
+
+def main():
+    id_dict = retrieve_ids(skip_check=False)
+    
+    group_id_dev = id_dict.get("group_id_dev")
+    report_id_pdf_dev = id_dict.get("report_id_pdf_dev")
+    bearer = id_dict.get("bearer")
+
+    reports_to_exports = retrieve_business_ids()
+
+    exporter = ReportExporter(bearer, group_id_dev, report_id_pdf_dev)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
+        futures = [
+            executor.submit(exporter.get_export_id, report["business_id"], report["language"])
+            for report in reports_to_exports
+        ]
+        concurrent.futures.wait(futures)
+
+    logging.info("All PDFs exported")
+
+def create_bearer(tenant_id: str, client_id: str, client_secret: str) -> None:
+    """
+    Creates a bearer token for authentication (if the one in the txt-file has expired).
+
+    Parameters
+    ----------
+    tenant_id : str
+        The tenant ID for the Azure AD.
+    client_id : str
+        The client ID for the application.
+    client_secret : str
+        The client secret for the application.
+
+    Returns
+    -------
+    None
+    """
+    url = f'https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token'
+    body = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "scope": "https://analysis.windows.net/powerbi/api/.default"
+    }
+    response = requests.post(url, data=body)
+    res = response.json()
+
+    bearer_to_file(res["access_token"])
+
+def bearer_to_file(bearer: str) -> None:
+    """
+    Saves the bearer token to a file with an expiration time.
+
+    Parameters
+    ----------
+    bearer : str
+        The bearer token for authentication.
+
+    Returns
+    -------
+    None
+    """
+    now = datetime.now()
+    expiry = now + timedelta(hours=1)
+    id_dict = retrieve_ids(skip_check=True)
+    with open("ids.txt", "w") as f:
+        for key, value in id_dict.items():
+            if key == "bearer":
+                f.write(f"bearer,{bearer},{expiry}\n")
+            else:
+                f.write(f"{key},{value}\n")
+
+def retrieve_ids(skip_check: bool) -> dict:
+    """
+    Retrieves IDs and bearer token from a file.
+
+    Parameters
+    ----------
+    skip_check : bool
+        If True, skips the bearer token expiration check to avoid infinte loop.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the retrieved IDs and bearer token.
+    """
+    id_dict = {}
+    with open("ids.txt", "r") as f:
+        client_id = id_dict.get("client_id")
+        client_secret = id_dict.get("client_secret")
+        tenant_id = id_dict.get("tenant_id")
+        for line in f:
+            line = line.strip("\n")
+            line = line.split(",")
+            line_str = line[0].strip()
+            line_id = line[1].strip()
+            match line_str:
+                case "client_id":
+                    id_dict[line_str] = line_id
+                    client_id = line_id
+                case "client_secret":
+                    id_dict[line_str] = line_id
+                    client_secret = line_id
+                case "tenant_id":
+                    id_dict[line_str] = line_id
+                    tenant_id = line_id
+                case "group_id_dev":
+                    id_dict[line_str] = line_id
+                case "report_id_pdf_dev":
+                    id_dict[line_str] = line_id
+                case "bearer":
+                    bearer_expiry = line[2]
+                    bearer_expiry = datetime.strptime(bearer_expiry, "%Y-%m-%d %H:%M:%S.%f")
+                    now = datetime.now()
+                    if now > bearer_expiry and not skip_check: #If time now is larger than the expiry in txt then the bearer has expired
+                        create_bearer(tenant_id, client_id, client_secret)
+                    id_dict[line_str] = line_id
+    return id_dict
+
+def retrieve_business_ids() -> list[dict]:
+    """
+    Retrieves business IDs from a CSV file, used for testing.
+
+    Returns
+    -------
+    list
+        A list of dictionaries containing business IDs and language settings.
+    """
+    business_id_list = []
+    with open('business_ids.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\n')
+        for row in reader:
+            id = ', '.join(row).strip()
+            business_id_list.append({"business_id": id, "language": "fi-FI"}) #Only Finnish reports are now used
+    return business_id_list
+
+if __name__ == "__main__":
+    main()
